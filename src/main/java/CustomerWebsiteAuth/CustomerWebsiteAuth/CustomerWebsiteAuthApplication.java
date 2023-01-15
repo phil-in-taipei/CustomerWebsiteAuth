6b8527d75fa639dcf8b9f9c61ff301c4f548aec7
mikalikahi <sweeney.phil@gmail.com>
2023-01-15 17:47:38 +0800

    first commit

---
+++ src/main/java/CustomerWebsiteAuth/CustomerWebsiteAuth/CustomerWebsiteAuthApplication.java
@@ -0,0 +1,13 @@
+package CustomerWebsiteAuth.CustomerWebsiteAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerWebsiteAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerWebsiteAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CustomerWebsiteAuth/CustomerWebsiteAuth/CustomerWebsiteAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package CustomerWebsiteAuth.CustomerWebsiteAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerWebsiteAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
