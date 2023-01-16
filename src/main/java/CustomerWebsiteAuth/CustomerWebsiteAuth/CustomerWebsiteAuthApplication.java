394efafb5080ff150e52e3ed3726edfba539869e
mikalikahi <sweeney.phil@gmail.com>
2023-01-16 15:56:22 +0800

    Beginning of command line runner logic to populate db with a few sample values and security configurations (general and methods)

--- src/main/java/CustomerWebsiteAuth/CustomerWebsiteAuth/CustomerWebsiteAuthApplication.java
+++ src/main/java/CustomerWebsiteAuth/CustomerWebsiteAuth/CustomerWebsiteAuthApplication.java
@@ -1,15 +1,34 @@
 package CustomerWebsiteAuth.CustomerWebsiteAuth;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerWebsiteAuthApplication {
+public class CustomerWebsiteAuthApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerWebsiteAuthApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		/*
+		Authority userAuth = Authority.builder().authority(RoleEnum.ROLE_USER).build();
+		Authority adminAuth = Authority.builder().authority(RoleEnum.ROLE_ADMIN).build();
+		if (authorityRepo.findAll().isEmpty()) {
+			authorityRepo.save(userAuth);
+			authorityRepo.save(adminAuth);
+		}
 
+		if (userPrincipalRepo.findAll().isEmpty()) {
+			userPrincipalRepo.save(
+					new UserPrincipal("USER", passwordEncoder.encode("user"),
+							Collections.singletonList(userAuth))
+			);
+		}
+
+		 */
+	}
 
 }
